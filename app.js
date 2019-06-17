5b87317f81db65dd203dab38b4ed9de0271f1657
williamestrada <willy.estrada81@gmail.com>
2019-06-17 13:21:39 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+let newItems = [];
+let workItems = [];
+let day = date.getDate();
+
+
+app.get("/", (req, res) => {
+  res.render("list", { today: day, newItems: newItems, listName: "William" });
+});
+
+app.post("/", (req, res) => {
+
+  let newItem = req.body.item;
+console.log(req.body.list)
+  if (req.body.list === "Work") {
+    workItems.push(newItem);
+    res.redirect("/work");
+  } else {
+    newItems.push(newItem);
+    res.redirect("/");
+  }
+
+ 
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { today: day, newItems: workItems, listName: "Work" });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
